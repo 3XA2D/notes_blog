d18be7554008f537d91fc4e6948ef88892c2a3d3
3XA2D <fstar2512@gmail.com>
2023-09-27 18:36:36 +0700

    Deploy stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://3xa2d.github.io',
+  base: '/notes_blog'
+});
