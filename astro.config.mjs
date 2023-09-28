340c66e8dd38d3988642ed7dd20bc7111efdc9ea
3XA2D <fstar2512@gmail.com>
2023-09-28 11:51:29 +0700

    Try to fix deployment 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://3xa2d.github.io',
-  base: '/',
+  base: '/notes-blog',
   markdown: {
         remarkPlugins: [
             'remark-math',
