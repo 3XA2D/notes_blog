e968c4e59e0d0be236780aedd787b58dfa5608f7
3XA2D <fstar2512@gmail.com>
2023-09-28 10:52:27 +0700

    Try to fix deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,15 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://3xa2d.github.io',
+  base: '/',
+  markdown: {
+        remarkPlugins: [
+            'remark-math',
+        ],
+        rehypePlugins: [
+            ['rehype-katex', {
+            // Katex plugin options
+            }]
+        ]
+    },
 });
